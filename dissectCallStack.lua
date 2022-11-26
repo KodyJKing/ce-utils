71081152b9350191490cfabc88927650a6bf112e
kjk <kody.j.king@gmail.com>
2022-11-26 04:06:52 -0800

    Created dissect call stack form and implemented stack snapshotting.

--- dissectCallStack.lua
+++ dissectCallStack.lua
@@ -1,7 +1,20 @@
-local dissectCallStack = {}
+local module = {}
 
-local function cwd()
-    return io.popen "cd":read '*l'
+------------------
+-- Misc Helpers --
+------------------
+
+local function toHex(n) return string.format("%x", n):upper() end
+
+local function printHex(n) print(toHex(n)) end
+
+local function cwd() return io.popen "cd":read '*l' end
+
+local function getFrameBase()
+    if targetIs64Bit() then
+        return RBP
+    end
+    return EBP
 end
 
 local pathsep
@@ -16,10 +29,14 @@ local root
 if dev then
     root = cwd() .. pathsep
 else
-    root = getAutoRunPath() .. 'ceutils' .. pathsep
+    -- root = getAutoRunPath() .. 'ceutils' .. pathsep
+    root = getAutoRunPath() .. pathsep
 end
 local formPath = root .. pathsep .. 'forms' .. pathsep
--- print(root)
+
+--------------------
+--- Form Helpers ---
+--------------------
 
 local function findSection(menu, n)
     local i = 0
@@ -37,15 +54,93 @@ local function insertInSection(menu, sectionIndex, offset, menuItem)
     menu.Items.insert(findSection(menu, sectionIndex) + offset, menuItem)
 end
 
-local function applyExtension()
+------------
+-- Module --
+------------
+
+function module.applyExtension()
     local mv = getMemoryViewForm()
     local mi = createMenuItem(mv.Menu)
     mi.Caption = "Dissect call stack"
     mi.Shortcut = 'Ctrl+Shift+K'
     mi.ImageIndex = 64
-    mi.OnClick = function() print("Not implemented!") end
+    mi.OnClick = module.createSession
     insertInSection(mv.debuggerpopup, 3, -1, mi)
 end
 
-local frm = createFormFromFile(formPath .. "DissectCallStack.FRM")
-frm.visible = true
+function module.createSession()
+    local session = {
+        onClose = {},
+        dialog = createFormFromFile(formPath .. "DissectCallStack.FRM"),
+        resultPointers = {},
+    }
+
+    function session.addCleanup(callback)
+        table.insert(session.onClose, callback)
+    end
+
+    function session.cleanup()
+        for i, callback in ipairs(session.onClose) do
+            callback()
+        end
+        return false
+    end
+
+    function session.close()
+        session.dialog.close()
+    end
+
+    function session.start()
+        if session.started then return end
+        session.started = true
+        session.dialog.findComponentByName("Start").Enabled = false
+        session.dialog.findComponentByName("Inputs").Enabled = false
+
+        session.maxCalls = tonumber(session.dialog.findComponentByName("MaxCalls").Text)
+        session.snapshotSize = tonumber(session.dialog.findComponentByName("SnapshotSize").Text)
+
+        -- print(session.address, session.maxCalls, session.snapshotSize)
+
+        debug_setBreakpoint(session.address, function()
+            print("Saving snapshot...")
+            local bp = getFrameBase()
+            local resultPtr = copyMemory(bp, session.snapshotSize)
+            table.insert(session.resultPointers, resultPtr)
+        end)
+    end
+
+    function session.stop()
+        if session.stopped then return end
+        session.stopped = true
+        session.dialog.findComponentByName("Stop").Enabled = false
+
+        debug_removeBreakpoint(session.address)
+    end
+
+    session.addCleanup(session.stop)
+    session.addCleanup(function()
+        for i, ptr in ipairs(session.resultPointers) do
+            print("Deallocating", toHex(ptr))
+            deAlloc(ptr)
+        end
+    end)
+    session.addCleanup(function() print("Cleaned up!") end)
+
+    local mv = getMemoryViewForm()
+    local a = mv.DisassemblerView.SelectedAddress
+    local b = mv.DisassemblerView.SelectedAddress2 or a
+    session.address = math.min(a, b)
+
+    session.dialog.findComponentByName("Start").OnClick = session.start
+    session.dialog.findComponentByName("Close").OnClick = session.close
+    session.dialog.findComponentByName("Stop").OnClick = session.stop
+    session.dialog.visible = true
+
+    session.dialog.OnClose = function()
+        session.cleanup()
+        session.dialog.OnClose = nil
+        session.dialog.close()
+    end
+end
+
+module.applyExtension()
