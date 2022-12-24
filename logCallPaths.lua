dc71ecdfa068804556bd43e2bc27447167b005ef
KJK <kody.j.king@gmail.com>
2022-12-23 23:39:35 -0800

    Progress on call chain logger.

--- logCallPaths.lua
+++ logCallPaths.lua
@@ -60,18 +60,6 @@ function getRegionMap()
     return _cachedRegionMap
 end
 
---------------------------------------------------------------
-
-function createCallChainLogForm(address)
-
-    local form = createFormFromFile(common.formPath .. "LogCallChains.FRM")
-
-    if address then
-        form.findComponentByName("Address").Text = address
-    end
-
-end
-
 function getCallChain(regionMap, stackLimit)
 
     local ptrSize = common.getPointerSize()
@@ -96,6 +84,114 @@ function getCallChain(regionMap, stackLimit)
 
 end
 
+--------------------------------------------------------------
+
+-- createCallChainLogForm(0x004EF49F)
+function createCallChainLogForm(initialAddress)
+
+    local regionMap = getRegionMap()
+
+    -- Form components --
+    local form = createFormFromFile(common.formPath .. "LogCallChains.FRM")
+    local addressInput = form.findComponentByName("Address")
+    local callLimitInput = form.findComponentByName("CallLimit")
+    local stackLimitInput = form.findComponentByName("StackLimit")
+    local listView = form.findComponentByName("List")
+    local optionsGroup = form.findComponentByName("Options")
+
+    -- Add caller columns
+    local NUM_CALLER_COLUMNS = 10
+    for i = 1, NUM_CALLER_COLUMNS do
+        local column = listView.Columns.add()
+        column.Caption = "Caller " .. i
+        column.AutoSize = true
+    end
+    ---------------------
+
+    if initialAddress then
+        addressInput.Text = toHex(initialAddress)
+    end
+
+    -- State --
+
+    local attatchedAddress = nil
+    local callChainRecords = {}
+
+    function getBreakAddress() return tonumber("0x" .. addressInput.Text) end
+
+    function getCallLimit() return tonumber(callLimitInput.Text) end
+
+    function getStackLimit() return tonumber(stackLimitInput.Text) end
+
+    -----------
+
+    function attach()
+        if attatchedAddress then return end
+        attatchedAddress = getBreakAddress()
+        optionsGroup.enabled = false
+
+        -- print("Attaching to", toHex(attatchedAddress))
+
+        local callLimit = getCallLimit()
+        local stackLimit = getStackLimit()
+
+        local calls = 0
+        debug_setBreakpoint(
+            attatchedAddress,
+            function()
+                addCallChain(getCallChain(regionMap, stackLimit))
+                calls = calls + 1
+                if calls >= callLimit then detatch() end
+            end
+        )
+    end
+
+    function detatch()
+        -- print("Detaching")
+        debug_removeBreakpoint(attatchedAddress)
+        optionsGroup.enabled = true
+        attatchedAddress = nil
+    end
+
+    function toggleAttach()
+        if attatchedAddress then
+            detatch()
+        else
+            attach()
+        end
+    end
+
+    -----------
+
+    function addCallChain(chain)
+        local lines = pad(attatchedAddress) .. "\n" .. table.concat(functional.map(chain, pad), "\n")
+
+        local record = callChainRecords[lines] or { count = 0 }
+        record.count = record.count + 1
+
+        if record.count == 1 then
+            callChainRecords[lines] = record
+            record.item = listView.Items.Add()
+            record.item.SubItems.text = lines
+        end
+        record.item.Caption = tostring(record.count)
+    end
+
+    -----------
+
+    form.findComponentByName("ToggleBreakpoint").OnClick = toggleAttach
+    form.OnClose = function()
+        detatch()
+        form.Destroy()
+    end
+
+    -----------
+
+    form.visible = true
+
+end
+
+-- printCallPaths(0x004EF49F)
 function printCallPaths(address, options)
     options = options or {}
     local callLimit = options.callLimit or 10
@@ -131,5 +227,3 @@ function printCallPaths(address, options)
     end)
 
 end
-
--- printCallPaths(0x004EF49F)
